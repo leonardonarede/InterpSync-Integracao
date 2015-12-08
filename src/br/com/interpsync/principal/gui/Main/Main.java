6011a4f958eaf63ba8c6681a72bceee39fd47ff9
leonardonarede <leonardonarede@gmail.com>
2015-12-08 12:06:41 -0300

    Publicando

---
+++ src/br/com/interpsync/principal/gui/Main/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.interpsync.principal.gui.Main;
+import br.com.interpsync.controller.Integracao;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class Main {
+    public static void main(String[] args) {
+        int tempo = 300;
+        int periodo = 30000;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask()
+        {
+           public void run()
+           {
+            Integracao integra = new Integracao();
+            integra.executar();
+               System.out.println("Teste");
+           }
+        }, tempo, periodo);
+
+    }
+}
